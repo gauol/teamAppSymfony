504c44accb9b03ae5fb8353a4423041285fdef6c
patryk <pgalczyk@gmail.com>
2018-02-24 03:48:44 +0100

    Dzialajace logowanie.

---
+++ src/AppBundle/Entity/Declaration.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Declaration
+ *
+ * @ORM\Table(name="declaration")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\DeclarationRepository")
+ */
+class Declaration
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="note", type="text", nullable=true)
+     */
+    private $note;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set note
+     *
+     * @param string $note
+     *
+     * @return Declaration
+     */
+    public function setNote($note)
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    /**
+     * Get note
+     *
+     * @return string
+     */
+    public function getNote()
+    {
+        return $this->note;
+    }
+}
+
